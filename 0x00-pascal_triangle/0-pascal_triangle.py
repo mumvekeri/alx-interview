8b722a7a9aa2b5d27c579e04248a97c0baf052aa
mumvekeri <mumvekerichidochashe@gmail.com>
2024-04-23 06:13:55 -0700

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def generate_pascals_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+
+def pascal_triangle(n):
+    return generate_pascals_triangle(n)
+
