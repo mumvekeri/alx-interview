95637e8d440c92d316086897710a0b7024be6a20
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-23 15:30:23 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,11 +6,9 @@ def generate_pascals_triangle(n):
     triangle = []
     for i in range(n):
         row = [1] * (i + 1)
-        for j in range(1, i):
+        for j in range(1, len(row) - 1):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
     return triangle
 
-def pascal_triangle(n):
-    return generate_pascals_triangle(n)
 
