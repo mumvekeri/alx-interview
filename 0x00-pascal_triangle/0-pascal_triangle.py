905cba10381b0de6befe95b8d54d96bca8cf6137
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-04-23 18:30:38 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-def generate_pascals_triangle(n):
+"""Pascal Triangle Interview Challenge"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of numbers
+    representing the Pascal triangle"""
     if n <= 0:
         return []
 
-    triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, len(row) - 1):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-    return triangle
+    triangle = [0] * n
 
+    for i in range(n):
+        # Define a row and fill first and last index with 1
+        row = [0] * (i+1)
+        row[0] = 1
+        row[len(row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(row):
+                a = triangle[i - 1][j]
+                b = triangle[i - 1][j - 1]
+                row[j] = a + b
+
+        triangle[i] = row
+
+    return triangle
