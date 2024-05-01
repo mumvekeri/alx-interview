fad926683f9dbc94f7954eca85ec1715fe29db42
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-01 22:43:33 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-'''LockBoxes Challenge'''
-
 
 def canUnlockAll(boxes):
-    '''determines if all the boxes can be opened or not
-    Returns:
-        True: all boxes can be opened
-        False: not all boxes can be opened
-    '''
     length = len(boxes)
     keys = set()
-    opened_boxes = []
-    i = 0
+    opened_boxes = set()
+    stack = [0]
 
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
+    while stack:
+        box_index = stack.pop()
+        opened_boxes.add(box_index)
+        keys.update(boxes[box_index])
+        
         for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
-            break
+            if key < length and key not in opened_boxes:
+                stack.append(key)
 
-    for i in range(length):
-        if i not in opened_boxes and i != 0:
-            return False
-    return True
+    return len(opened_boxes) == length
+
