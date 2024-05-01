5702e375dd3c47f0628096ef4ee1b5660c2dc761
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-01 22:52:40 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,24 +11,19 @@ def canUnlockAll(boxes):
         bool: True if all boxes can be opened, False otherwise.
     '''
 
-    # Total number of boxes
     n = len(boxes)
 
-    # Set to keep track of opened boxes
     opened_boxes = set()
 
-    # Stack to perform DFS (starting with box 0)
     stack = [0]
 
     while stack:
         box_index = stack.pop()
         opened_boxes.add(box_index)
 
-        # Iterate through keys in the current box
         for key in boxes[box_index]:
             if key < n and key not in opened_boxes:
                 stack.append(key)
 
-    # Check if all boxes can be opened (i.e., all boxes are in the opened set)
     return len(opened_boxes) == n
 
