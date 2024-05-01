bbb856eb2b7fa6383b9f6ca9ebf1316d95c0e3a1
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-01 22:41:12 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+'''LockBoxes Challenge'''
+
 
 def canUnlockAll(boxes):
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True
-    keys = [0]
-    
-    while keys:
-        new_keys = []
-        for key in keys:
-            if unlocked[key] == False:
-                unlocked[key] = True
-            for new_key in boxes[key]:
-                if new_key < len(boxes) and not unlocked[new_key]:
-                    new_keys.append(new_key)
-        keys = new_keys
-    
-    return all(unlocked)
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-# Example of usage:
-# print(canUnlockAll([[1], [2], [3], [4], []])) # Returns True
-# print(canUnlockAll([[1, 2], [2, 3], [3], []])) # Returns False
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
