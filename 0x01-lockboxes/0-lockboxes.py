55cee53c563b8ca9ed0569debb1e7be3896c9a9b
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-01 22:28:29 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,38 @@
 def canUnlockAll(boxes):
-    # Initialize a set to keep track of unlocked boxes
-    unlocked = set()
-    unlocked.add(0)
-    # Initialize a list to process keys
-    keys = []
-    keys.extend(boxes[0])
+    if not boxes:
+        return False
     
-    # Process keys to unlock boxes
-    while keys:
-        new_key = keys.pop()
-        if new_key not in unlocked and new_key < len(boxes):
-            unlocked.add(new_key)
-            keys.extend(boxes[new_key])
-    
-    # If the number of unlocked boxes equals the number of boxes, return True
-    return len(unlocked) == len(boxes)
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]  # Start BFS from box 0
+
+    while queue:
+        current_box = queue.pop(0)  # Get the front of the queue
+        keys = boxes[current_box]    # Keys in the current box
+
+        for key in keys:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    # Check if all boxes are visited
+    return all(visited)
+
+# Example usage
+boxes = [
+    [1],        # Box 0 has key to Box 1
+    [2],        # Box 1 has key to Box 2
+    [3],        # Box 2 has key to Box 3
+    [],         # Box 3 has no keys
+]
+print(canUnlockAll(boxes))  # Output: True
+
+boxes_with_unreachable = [
+    [1, 2],     # Box 0 has keys to Box 1 and Box 2
+    [3],        # Box 1 has key to Box 3
+    [],         # Box 2 has no keys
+    [0],        # Box 3 has key back to Box 0 (cyclic, but cannot reach all)
+]
+print(canUnlockAll(boxes_with_unreachable))  # Output: False
+
