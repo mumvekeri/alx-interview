d958e01d5f4eb7cc3c071c9306d4fe3e66e84ac3
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-01 22:44:49 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
+'''LockBoxes Challenge'''
 
 def canUnlockAll(boxes):
-    length = len(boxes)
-    keys = set()
+    '''Determines if all the boxes can be opened or not.
+
+    Args:
+        boxes (list of list): The list of boxes where each box contains a list of keys.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    '''
+
+    # Total number of boxes
+    n = len(boxes)
+
+    # Set to keep track of opened boxes
     opened_boxes = set()
+
+    # Stack to perform DFS (starting with box 0)
     stack = [0]
 
     while stack:
         box_index = stack.pop()
         opened_boxes.add(box_index)
-        keys.update(boxes[box_index])
-        
-        for key in keys:
-            if key < length and key not in opened_boxes:
+
+        # Iterate through keys in the current box
+        for key in boxes[box_index]:
+            if key < n and key not in opened_boxes:
                 stack.append(key)
 
-    return len(opened_boxes) == length
+    # Check if all boxes can be opened (i.e., all boxes are in the opened set)
+    return len(opened_boxes) == n
 
