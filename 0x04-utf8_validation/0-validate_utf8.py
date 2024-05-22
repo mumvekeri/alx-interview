e902445122ce3ed84b6eb9bad2b71f7b795c53f5
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-22 18:22:39 +0200

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,15 @@
-#!/usr/bin/env python3
-"""UTF-8 MODULE
+#!/usr/bin/python3
+"""
+Main file for testing
 """
 
+validUTF8 = __import__('0-validate_utf8').validUTF8
 
-def validUTF8(data):
-    def check_following_bytes(start, count):
-        for i in range(start + 1, start + count + 1):
-            if i >= len(data) or (data[i] & 0b11000000) != 0b10000000:
-                return False
-        return True
-    
-    i = 0
-    while i < len(data):
-        byte = data[i]
-        if (byte & 0b10000000) == 0:
-            i += 1
-            continue
-        elif (byte & 0b11100000) == 0b11000000:
-            if not check_following_bytes(i, 1):
-                return False
-            i += 2
-        elif (byte & 0b11110000) == 0b11100000:
-            if not check_following_bytes(i, 2):
-                return False
-            i += 3
-        elif (byte & 0b11111000) == 0b11110000:
-            if not check_following_bytes(i, 3):
-                return False
-            i += 4
-        else:
-            return False
-    return True
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
