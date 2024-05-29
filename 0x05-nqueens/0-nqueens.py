3f0d09530140334a77bb51ab4f15081cf6486450
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-29 23:09:01 +0200

    Create 0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""N queens"""
+import sys
+
+def is_valid(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def solve_nqueens_util(board, col, solutions):
+    """Utilize backtracking to find all solutions."""
+    if col >= len(board):
+        # A solution is found, convert it to the required format
+        solution = []
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for i in range(len(board)):
+        if is_valid(board, i, col):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, solutions)
+            board[i][col] = 0  # backtrack
+
+def solve_nqueens(n):
+    """Solve the N Queens problem and return all possible solutions."""
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+    solve_nqueens_util(board, 0, solutions)
+    return solutions
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_nqueens(n)
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    main()
